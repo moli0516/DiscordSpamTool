6d2ba0c05d0bd22a6dbd5c2ad8bad0f80b65d371
moli <kyeung516@gmail.com>
2022-08-01 15:06:40 +0800

    update_1/8

---
+++ Main.py
@@ -0,0 +1,95 @@
+from tkinter import messagebox
+import tkinter as tk
+import pyautogui
+import time
+import sys
+import json
+
+class gui:
+    def __init__(self):
+
+        def validate(P):
+            print(P)
+            if str.isdigit(P) or P == '':
+                return True
+            else:
+                return False
+        
+        self.window = tk.Tk()
+        self.window.title('Word Macro')
+        self.window.geometry('600x700')
+        self.window.configure(background='azure3')
+        self.div_size = 35
+        self.div_height = 7
+        self.div_1 = tk.Label(self.window, text="WordMacro", font=('Arial', 12), height=5, background='azure3')
+        self.div_1.grid(column=0,row=0,columnspan=2)
+        self.div_3 = tk.Label(self.window, text="Wassup", font=('Arial', 12), width=self.div_size, height=self.div_height, background='azure3')
+        self.div_3.grid(column=0,row=3,sticky="W",rowspan=2)
+        self.var1 = tk.StringVar()
+        self.msg = tk.Entry(self.window, textvariable=self.var1)
+        self.msg.grid(column=1,row=3,padx=5,sticky="E")
+        self.button1 = tk.Button(self.window, text='Enter', command=self.button_event_1, font=('Arial', 12))
+        self.button1.grid(row=4, column=1)
+        self.div_4 = tk.Label(self.window, text="how many times you want?", font=('Arial', 12), width=self.div_size, height=self.div_height, background='azure3')
+        self.div_4.grid(column=0,row=5,sticky="W",rowspan=2)
+        vcmd = self.window.register(validate)
+        self.var2 = tk.IntVar()
+        self.times = tk.Entry(self.window, textvariable=self.var2, validate="key", validatecommand=(vcmd, '%P'))
+        self.times.grid(column=1,row=5,padx=5,sticky="E")
+        self.button2 = tk.Button(self.window, text='Enter', command=self.button_event_2, font=('Arial', 12))
+        self.button2.grid(row=6, column=1)
+        self.button3 = tk.Button(self.window, text='GO!!', command=self.button_event_3, font=('Arial', 12))
+        self.button3.grid(row=7, column=0,pady=35)
+        self.button4 = tk.Button(self.window, text='Quit', command=self.button_event_4, font=('Arial', 12))
+        self.button4.grid(row=7, column=1,pady=35)
+        self.window.mainloop()
+
+    def spam(self):
+        msg1 = self.var1.get()
+        pyautogui.typewrite(msg1)
+        time.sleep(0.2)
+        pyautogui.press("enter")
+        time.sleep(0.2)
+
+    def button_event_1(self):
+        if self.var1.get() == '':
+            tk.messagebox.showerror('message', 'nothing is typed, type again')
+        else:
+            tk.messagebox.showinfo('message', 'done')
+
+    def button_event_2(self):
+        if self.var2.get() == '':
+            tk.messagebox.showerror('message', 'nothing is typed, type again')
+        else:
+            tk.messagebox.showinfo('message', 'done')
+
+    def button_event_3(self):
+        with open("path.json") as f:
+            data = json.load(f)
+            print(data)
+        print("Opening discord...")
+        time.sleep(1)
+        pyautogui.hotkey('win','r')
+        time.sleep(1)
+        pyautogui.typewrite(data['path'])
+        time.sleep(0.5)
+        pyautogui.press("enter")
+        time.sleep(0.5)
+        pyautogui.click(499,989)
+        time.sleep(1)
+        print("Process Success")
+        time.sleep(1)
+        print("Spaming the message...")
+        realTime = 0
+        times = self.var2.get()
+        while realTime < times:
+            self.spam()
+            realTime += 1
+            realTimeStr = str(realTime)
+            print("times: " + realTimeStr)
+        print("Process Success")
+
+    def button_event_4(self):
+        sys.exit()
+
+gui()
--- README.md
+++ README.md
@@ -8,9 +8,11 @@
 # Manual
 - Download the whole folder from github
 - Execute the EXE file
-- Enter the path of discord(Find discord in task manager in the detail part, then open it in explorer)
+- Enter the path of discord to the json file (Find discord in task manager in the detail part, then open it in explorer)
 - Enter the message and the times that you want the program to spam
 - Enjoy it
 
 # Remind
 - Always open the channel page before you use this program
+- Add a '\' before a '\' (just like C://a//b//c//Discord.exe)
+- Make sure only 1 `"` exist in the begining and the end of the data `path` (`"path"` not `""path""`)
